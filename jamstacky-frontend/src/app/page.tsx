63be981f5e149583cfea309c494796e88d95736c
CRea10r <125733574+CRea10r@users.noreply.github.com>
2025-01-03 11:01:26 +0530

    dynamic loaded component

--- jamstacky-frontend/src/app/page.tsx
+++ jamstacky-frontend/src/app/page.tsx
@@ -1,15 +1,7 @@
+import dynamic from "next/dynamic";
 import { client } from "./sanity/lib/sanityClient";
 import { PAGE_QUERY } from "./sanity/sanityQueries/pageQueries";
 import { Section } from "./sanity/types/pageType";
-import CaseStudySection from "./Components/Sections/CaseStudySection";
-import HeroSection from "./Components/Sections/HeroSection";
-import TechnologySection from "./Components/Sections/TechnologySection";
-import CompareSection from "./Components/Sections/CompareSection";
-import EdgeCaseSecction from "./Components/Sections/EdgeCaseSection";
-import TestimonialSection from "./Components/Sections/TestimonialSection";
-import BlogSection from "./Components/Sections/BlogSection";
-import SliderSection from "./Components/Sections/SliderSection";
-import ContactSection from "./Components/Sections/ContactSection";
 import { HeroSection as Hero } from "./sanity/types/sectionTypes/heroSection";
 import { CaseStudySection as CaseStudy } from "./sanity/types/sectionTypes/caseStudySection";
 import { TechnologySection as Technology } from "./sanity/types/sectionTypes/technologySection";
@@ -20,6 +12,16 @@ import { BlogSection as Blog } from "./sanity/types/sectionTypes/blogSection";
 import { SliderSection as Slider } from "./sanity/types/sectionTypes/sliderSection";
 import { ContactSection as Contact } from "./sanity/types/sectionTypes/contactSection";
 
+const HeroSection = dynamic(() => import("./Components/Sections/HeroSection"));
+const CaseStudySection = dynamic(() => import("./Components/Sections/CaseStudySection"));
+const TechnologySection = dynamic(() => import("./Components/Sections/TechnologySection"));
+const CompareSection = dynamic(() => import("./Components/Sections/CompareSection"));
+const EdgeCaseSection = dynamic(() => import("./Components/Sections/EdgeCaseSection"));
+const TestimonialSection = dynamic(() => import("./Components/Sections/TestimonialSection"));
+const BlogSection = dynamic(() => import("./Components/Sections/BlogSection"));
+const SliderSection = dynamic(() => import("./Components/Sections/SliderSection"));
+const ContactSection = dynamic(() => import("./Components/Sections/ContactSection"));
+
 export default async function Home() {
   const homePage = await client.fetch(PAGE_QUERY, { slug: 'home' });
   return (
@@ -27,23 +29,23 @@ export default async function Home() {
       {homePage.sectionsList.map((sectionItem: Section, sectionIndex: number) => {
         switch (sectionItem.type) {
           case "heroSection":
-            return <HeroSection key={`${sectionIndex}`} data={sectionItem as { sectionContent: Hero[] }} />;
+            return <HeroSection key={sectionIndex} data={sectionItem as { sectionContent: Hero[] }} />;
           case "caseStudySection":
-            return <CaseStudySection key={`${sectionIndex}`} data={sectionItem as { sectionContent: CaseStudy[] }} />;
+            return <CaseStudySection key={sectionIndex} data={sectionItem as { sectionContent: CaseStudy[] }} />;
           case "technologySection":
-            return <TechnologySection key={`${sectionIndex}`} data={sectionItem as { sectionContent: Technology[] }} />;
+            return <TechnologySection key={sectionIndex} data={sectionItem as { sectionContent: Technology[] }} />;
           case "compareSection":
-            return <CompareSection key={`${sectionIndex}`} data={sectionItem as { sectionContent: Compare[] }} />;
+            return <CompareSection key={sectionIndex} data={sectionItem as { sectionContent: Compare[] }} />;
           case "edgeCaseSection":
-            return <EdgeCaseSecction key={`${sectionIndex}`} data={sectionItem as { sectionContent: EdgeCase[] }} />; 
+            return <EdgeCaseSection key={sectionIndex} data={sectionItem as { sectionContent: EdgeCase[] }} />;
           case "testimonialSection":
-            return <TestimonialSection key={`${sectionIndex}`} data={sectionItem as { sectionContent: Testimonial[] }} />;
+            return <TestimonialSection key={sectionIndex} data={sectionItem as { sectionContent: Testimonial[] }} />;
           case "blogSection":
-            return <BlogSection key={`${sectionIndex}`} data={sectionItem as { sectionContent: Blog[] }} />;
+            return <BlogSection key={sectionIndex} data={sectionItem as { sectionContent: Blog[] }} />;
           case "sliderSection":
-            return <SliderSection key={`${sectionIndex}`} data={sectionItem as { sectionContent: Slider[] }} />;
+            return <SliderSection key={sectionIndex} data={sectionItem as { sectionContent: Slider[] }} />;
           case "contactSection":
-            return <ContactSection key={`${sectionIndex}`} data={sectionItem as { sectionContent: Contact[] }} />;
+            return <ContactSection key={sectionIndex} data={sectionItem as { sectionContent: Contact[] }} />;
           default:
             console.warn(`Unknown section type: ${sectionItem.type}`);
             return null;
